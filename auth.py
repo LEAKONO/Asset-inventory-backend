e3f0672096803194f74d773973fd6805422d3826
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2024-08-05 16:55:34 +0300

    changed authentication

--- auth.py
+++ auth.py
@@ -39,7 +39,7 @@ def login():
     user = User.query.filter_by(username=username).first()
     if user and user.check_password(password):
         access_token = create_access_token(identity={'id': user.id, 'role': user.role})
-        return jsonify({'access_token': access_token}), 200
+        return jsonify({'access_token': access_token, 'role':user.role}), 200
 
     return jsonify({'message': 'Invalid credentials'}), 401
 @auth.route('/logout', methods=['POST'])
