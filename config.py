898a0caa1ddbdfd0994c98e58d13e005bc595a3e
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2024-08-05 15:54:04 +0300

    changed config file

--- config.py
+++ config.py
@@ -2,15 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    if not SECRET_KEY:
-        raise ValueError("No SECRET_KEY set for Flask application")
-
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-    if not JWT_SECRET_KEY:
-        raise ValueError("No JWT_SECRET_KEY set for Flask application")
-
     JWT_ACCESS_TOKEN_EXPIRES = int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES', 3600))
 
     
